b4629342277b667a2856285ee06921af511e4b65
Leon Berenschot <leonb@beriedata.nl>
2009-12-03 15:33:31 +0100

    Adapt the wave_stream to net CreateImage class

--- wave_stream.rb
+++ wave_stream.rb
@@ -2,15 +2,27 @@
 
 # mpg123 -s -d 192 2009-11-15-1830.mp3 | ./wave_stream.rb
 
-@data = []
-@input_type = "Stream"
-@image_file = "generated_from_stream.png"
+require 'create_image'
+
+rate = 4
+
+data = []
+info = "Rate: %d" % rate
+input_type = "Stream"
+image_file = "generated_from_stream_#{rate}.png"
+
+t = Time.now
+
+# file=$stdin
 
 $stdin.each do |line| 
-  line.scan(/..../).each do |var| # is this the right way?
+  line.scan(/^.{4}/).each do |var| # is this the right way?
     first_channel_data, second_channel_data = var.unpack('ss')
-    @data << first_channel_data
+    data << first_channel_data
   end
 end
 
-require 'process'
+info << ", Samples: %d" % data.length
+
+CreateImage.new(data, info, image_file, input_type)
+puts "Time taken: %d" % [Time.now - t]
